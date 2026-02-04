f503f326e3b52fcb0c7dc45325a104132b8d84da
dlswilliam <santo@Project2.localdomain>
2026-02-04 00:35:23 -0500

    fixed

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,20 @@
+import "./WeatherForecast.css";
+import WeatherIcon from "./WeatherIcon";
+import WeatherData from "./WeatherData";
+
+export default function WeatherForecast({ forecast }) {
+  return (
+    <div className="weather">
+      <WeatherData
+        day={forecast.day}
+        conditions={forecast.conditions}
+        time={forecast.time}
+      />
+
+      <WeatherIcon
+        img={forecast.img}
+        imgAlt={forecast.imgAlt}
+      />
+    </div>
+  );
+}
